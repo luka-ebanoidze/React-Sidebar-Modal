543da907896d3f2d531e45553aa9a3a8b979ab26
luka-ebanoidze <ebanoidzelukaa@gmail.com>
2023-04-02 23:37:57 +0400

    first commit

---
+++ src/context.js
@@ -0,0 +1,48 @@
+import React, { useState, useContext } from "react";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openSideBar = () => {
+    setIsSideBarOpen(true);
+    console.log(123);
+  };
+
+  const closeSideBar = () => {
+    setIsSideBarOpen(false);
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isModalOpen,
+        isSideBarOpen,
+        openModal,
+        openSideBar,
+        closeModal,
+        closeSideBar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+// curstom hook
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
